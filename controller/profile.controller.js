d130af557ec7b76e5c718fb885dff123253c8db6
2093ab <gil2093ab@gmail.com>
2024-06-09 23:45:50 +0900

    docs: interest typo fix at modify profile

--- controller/profile.controller.js
+++ controller/profile.controller.js
@@ -1,5 +1,6 @@
 const prisma = require('../lib/prisma');
 const { LevelName } = require('@prisma/client');
+
 exports.createProfile = async (req, res) => {
   const profilePhoto = req.file
     ? `/uploads/profile/${req.file.filename}`
@@ -28,7 +29,7 @@ exports.updateProfile = async (req, res) => {
   const profilePhoto = req.file
     ? `/uploads/profile/${req.file.filename}`
     : '/uploads/profile/default.jpg';
-  const { nickname, bio, birthdate, interests } = req.body;
+  const { nickname, bio, birthdate, interest } = req.body;
   try {
     const profile = await prisma.profile.update({
       where: { userId: parseInt(userId) },
@@ -36,7 +37,7 @@ exports.updateProfile = async (req, res) => {
         nickname,
         bio,
         birthdate: birthdate ? new Date(birthdate) : null,
-        interests,
+        interest,
         profilePhoto,
       },
     });
